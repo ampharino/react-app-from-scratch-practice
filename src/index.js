4d2614eb1ea07431aabbbc658e5a0b1978704fe7
ampharino <ampharin@gmail.com>
2021-09-15 20:42:21 +0700

    Add another module just to test webpack functionality

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
+import sum from './sum';
+
 const greet = (name) => console.log(`Hello, ${name}`);
 
 greet('Jon Snow');
 
-const a = new Promise();
+// const a = new Promise();
+
+console.log(sum(2, 4));
