9cfd7dfddb8230b888a89bac0f7619eea848ae8e
ampharino <ampharin@gmail.com>
2021-09-17 22:00:54 +0700

    Add production mode for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,35 +3,38 @@ const path = require('path');
 const PATH_SORUCE = path.join(__dirname, './src');
 const PATH_DIST = path.join(__dirname, './dist');
 
-module.exports = {
-    mode: 'development',
-    entry: [path.join(PATH_SORUCE, './index.js')],
-    output: {
-        path: PATH_DIST,
-        filename: 'js/[name].[contenthash].js',
-        clean: true,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: [
-                            [
-                                '@babel/preset-env',
-                                {
-                                    debug: true,
-                                    useBuiltIns: 'usage',
-                                    corejs: 3,
-                                },
+module.exports = (env) => {
+    const environment = env.environment;
+    return {
+        mode: environment,
+        entry: [path.join(PATH_SORUCE, './index.js')],
+        output: {
+            path: PATH_DIST,
+            filename: 'js/[name].[contenthash].js',
+            clean: true,
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: [
+                                [
+                                    '@babel/preset-env',
+                                    {
+                                        debug: true,
+                                        useBuiltIns: 'usage',
+                                        corejs: 3,
+                                    },
+                                ],
                             ],
-                        ],
+                        },
                     },
                 },
-            },
-        ],
-    },
+            ],
+        },
+    };
 };
