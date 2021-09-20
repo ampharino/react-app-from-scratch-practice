65506524ec5edbd469f2a1c8acf1d2d7d00f597b
ampharino <ampharin@gmail.com>
2021-09-20 18:05:57 +0700

    Simplify commands and extract babel config to babelrc

--- webpack.config.js
+++ webpack.config.js
@@ -5,53 +5,40 @@ const PATH_SORUCE = path.join(__dirname, './src');
 const PATH_DIST = path.join(__dirname, './dist');
 
 module.exports = (env) => {
-    const environment = env.environment;
-    return {
-        mode: environment,
-        entry: [path.join(PATH_SORUCE, './index.js')],
-        output: {
-            path: PATH_DIST,
-            filename: 'js/[name].[contenthash].js',
-            clean: true,
+  const environment = env.environment;
+  return {
+    mode: environment,
+    entry: [path.join(PATH_SORUCE, './index.js')],
+    output: {
+      path: PATH_DIST,
+      filename: 'js/[name].[contenthash].js',
+      clean: true,
+    },
+    devServer: {
+      static: {
+        directory: PATH_DIST,
+      },
+      host: 'localhost',
+      port: 8080,
+      historyApiFallback: true,
+      client: {
+        overlay: {
+          errors: true,
+          warnings: true,
         },
-        devServer: {
-            static: {
-                directory: PATH_DIST,
-            },
-            host: 'localhost',
-            port: 8080,
-            historyApiFallback: true,
-            client: {
-                overlay: {
-                    errors: true,
-                    warnings: true,
-                },
-            },
+      },
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+          },
         },
-        module: {
-            rules: [
-                {
-                    test: /\.js$/,
-                    exclude: /node_modules/,
-                    use: {
-                        loader: 'babel-loader',
-                        options: {
-                            presets: [
-                                [
-                                    '@babel/preset-env',
-                                    {
-                                        debug: true,
-                                        useBuiltIns: 'usage',
-                                        corejs: 3,
-                                    },
-                                ],
-                                ['@babel/preset-react'],
-                            ],
-                        },
-                    },
-                },
-            ],
-        },
-        plugins: [new HtmlWebpackPlugin({ template: 'src/index.html' })],
-    };
+      ],
+    },
+    plugins: [new HtmlWebpackPlugin({ template: 'src/index.html' })],
+  };
 };
